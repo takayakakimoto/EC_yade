44df5500264fc11cbc6560863181878d72c6ebda
柿本貴哉 <takayakakimoto@takayakakimotoMacBook-Pro.local>
2019-05-13 14:50:46 +0900

    product_modelテスト

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "全カラムを指定しているとき" do
+    it "商品情報が作られる" do
+      product = create(:product)
+
+      expect(product).to be_valid
+    end
+  end
+
+  context "nameがない場合" do
+    it "エラーする" do
+      product = build(:product, name:nil)
+      product.valid?
+
+      expect(product.errors.messages[:name]).to include "can't be blank"
+    end
+  end
+
+  context "descriptionがない場合" do
+    it "エラーする" do
+      product = build(:product, description:nil)
+      product.valid?
+
+      expect(product.errors.messages[:description]).to include "can't be blank"
+    end
+  end
+
+  context "priceがない場合" do
+    it "エラーする" do
+      product = build(:product, price:nil)
+      product.valid?
+
+      expect(product.errors.messages[:price]).to include "can't be blank"
+    end
+  end
 end
