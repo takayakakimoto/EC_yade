c0e133f36cabe93780356405e0425bb1709d3d52
柿本貴哉 <takayakakimoto@takayakakimotoMacBook-Pro.local>
2019-05-15 00:25:09 +0900

    product_view修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-
   root to: "products#index"
 
-  resources :products
+  resources :products, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   if Rails.env.development? #開発環境の場合
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
