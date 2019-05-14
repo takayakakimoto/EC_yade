da654ad7c5282398a509466a8af72870857c9c45
柿本貴哉 <takayakakimoto@takayakakimotoMacBook-Pro.local>
2019-05-14 11:06:36 +0900

    product.rb修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
+  validates :image, presence: true
 end
