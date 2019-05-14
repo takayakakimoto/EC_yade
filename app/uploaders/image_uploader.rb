52bf42ba419941aa0a8b6fd0203daa14fc7442e3
柿本貴哉 <takayakakimoto@takayakakimotoMacBook-Pro.local>
2019-05-15 01:27:29 +0900

    image設定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,4 +1,12 @@
 class ImageUploader < ApplicationUploader
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
+  def accept_extensions
+    accept_list = extension_whitelist.map { |e| "image/#{e}" }
+    accept_list.join(",")
+  end
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
