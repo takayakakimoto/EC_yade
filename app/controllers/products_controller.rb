22737490e750937265a8f2a652689c69a463083c
柿本貴哉 <takayakakimoto@takayakakimotoMacBook-Pro.local>
2019-05-14 10:22:53 +0900

    product_new_create実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,24 @@
 class ProductsController < ApplicationController
   def index
-    @Products = Product.all
+    @products = Product.all
   end
 
   def show
   end
 
   def new
-    @Product = Product.new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to products_path
+  end
+
+  private
+
+  def product_params
+    params[:product].permit(:name, :description, :price)
   end
 end
