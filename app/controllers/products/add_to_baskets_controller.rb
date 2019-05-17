e6de327777145150e0407a0df00781e8d5619ff9
柿本貴哉 <takayakakimoto@takayakakimotoMacBook-Pro.local>
2019-05-17 22:33:29 +0900

    買い物カゴに入れる実装

--- app/controllers/products/add_to_baskets_controller.rb
+++ app/controllers/products/add_to_baskets_controller.rb
@@ -3,6 +3,6 @@ class Products::AddToBasketsController < Products::ApplicationController
   def create
     basket = current_user.prepare_basket
     product = Product.find(params[:product_id])
-    basket.basket_products.create!(product_id: product_id)
+    basket.basket_products.create!(product_id: product.id)
   end
 end
--- app/controllers/products/application_controller.rb
+++ app/controllers/products/application_controller.rb
@@ -1,1 +1,2 @@
 class Products::ApplicationController < ApplicationController
+end
