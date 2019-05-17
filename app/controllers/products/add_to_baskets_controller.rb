68a4174758a9ea2bce552b7f30f9749f5c8c4161
柿本貴哉 <takayakakimoto@takayakakimotoMacBook-Pro.local>
2019-05-17 21:32:10 +0900

    add_to_baskets_controller.rb修正

--- app/controllers/products/add_to_baskets_controller.rb
+++ app/controllers/products/add_to_baskets_controller.rb
@@ -2,5 +2,7 @@ class Products::AddToBasketsController < Products::ApplicationController
 
   def create
     basket = current_user.prepare_basket
+    product = Product.find(params[:product_id])
+    basket.basket_products.create!(product_id: product_id)
   end
 end
