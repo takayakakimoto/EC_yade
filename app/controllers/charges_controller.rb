8aa124ef3fb0148ac17f410564a64ebcc2766004
柿本貴哉 <takayakakimoto@takayakakimotoMacBook-Pro.local>
2019-05-23 00:45:53 +0900

    baskets/show修正

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,4 +1,13 @@
 class ChargesController < ApplicationController
   def create
+    Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+    token = params[:stripeToken]
+    Stripe::Charge.create({
+      amount: 999,
+      currency: 'jpy',
+      description: 'Example charge',
+      source: token
+    })
+    redirect_to root_path, notice: '決済に成功しました'
   end
 end
