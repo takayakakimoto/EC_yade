dbacc46f905c1c5a2ced74a385cce9db8d37d511
柿本貴哉 <takayakakimoto@takayakakimotoMacBook-Pro.local>
2019-05-22 21:03:45 +0900

    basket内の合計金額

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -4,5 +4,6 @@ class BasketsController < ApplicationController
   def show
     basket = current_user.prepare_basket
     @products = basket.products
+    @total_price = basket.total_price
   end
 end
--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,4 +1,14 @@
 class Basket < ApplicationRecord
   has_many :basket_products, dependent: :destroy
   has_many :products, through: :basket_products
+
+  def total_price
+		total_jpy_price
+  end
+
+  private
+  def total_jpy_price
+      jp_products = products
+      jp_products.sum(:price)
+  end
 end
