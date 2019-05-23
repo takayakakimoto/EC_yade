bebc95e402a4bfec82e1d4409c074fd3db9d30fc
柿本貴哉 <takayakakimoto@takayakakimotoMacBook-Pro.local>
2019-05-23 15:45:23 +0900

    購入フローリファクタリング

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,19 +4,7 @@ class ChargesController < ApplicationController
     token = params[:stripeToken]
     product_ids = params[:product_ids].map(&:to_i)
 
-    #正しい金額で決済する
-    total = current_user.basket.total_price(product_ids: product_ids)
-
-    #削除
-    basket_products = current_user.basket.basket_products.where(product_id: product_ids)
-    basket_products.each(&:destroy!)
-
-    Stripe::Charge.create({
-      amount: total,
-      currency: 'jpy',
-      description: 'Example charge',
-      source: token
-    })
+    current_user.checkout!(token, product_ids: product_ids)
     redirect_to root_path, notice: '決済に成功しました'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,13 @@ class User < ApplicationRecord
   def prepare_basket
     basket || create_basket
   end
+
+  def checkout!(token, product_ids:)
+    #正しい金額
+    total = basket.total_price(product_ids: product_ids)
+    #削除
+    basket_products = basket.basket_products.where(product_id: product_ids)
+    basket_products.each(&:destroy!)
+    Charge.create!(total, token)
+  end
 end
