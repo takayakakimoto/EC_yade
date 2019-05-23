ee1377ba609f00585118555ff0b8a7ae0993b601
柿本貴哉 <takayakakimoto@takayakakimotoMacBook-Pro.local>
2019-05-23 15:50:50 +0900

    購入履歴保存処理

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,23 @@ class User < ApplicationRecord
     basket || create_basket
   end
 
+  def prepare_purchase_record
+    purchase_record || create_purchase_record
+  end
+
   def checkout!(token, product_ids:)
     #正しい金額
     total = basket.total_price(product_ids: product_ids)
     #削除
     basket_products = basket.basket_products.where(product_id: product_ids)
     basket_products.each(&:destroy!)
+
+    ##購入履歴
+    purchase_record = prepare_purchase_record
+    ids = product_ids.map { |id| { product_id: id } }
+    purchase_record.purchase_record_products.create!(ids)
+
+
     Charge.create!(total, token)
   end
 end
