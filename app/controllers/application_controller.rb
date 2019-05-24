76aaa106f324c14b6d1a424b5ceaefe0ec48dff0
柿本貴哉 <takayakakimoto@takayakakimotoMacBook-Pro.local>
2019-05-25 02:08:11 +0900

    キーワード検索

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :search  #protect_from_forgery with: :exceptionより後におく。
   def search
     @search = Product.ransack(params[:q])
-    @products = @search.result
+    @search_products = @search.result.page(params[:page])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
 
 
 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   if Rails.env.development? #開発環境の場合
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
