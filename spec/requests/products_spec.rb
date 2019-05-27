709e531cc71bab57ad59e662b7db775ee79c5a0a
takayakakimoto <godkami420@yahoo.co.jp>
2019-05-27 16:36:56 +0900

    rspec

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -2,9 +2,21 @@ require 'rails_helper'
 
 RSpec.describe "Products", type: :request do
   describe "GET /products" do
-    it "works! (now write some real specs)" do
-      get products_path
+    subject { get(products_path) }
+    before do
+      create_list(:product,3)
+    end
+    it "リクエストが200になる" do
+      subject
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "GET /products/:id" do
+    subject { get(product_path(product_id)) }
+   context "指定したidの商品が存在する場合" do
+    it "商品の値が取得できる" do
+    end
+  end
+end
 end
